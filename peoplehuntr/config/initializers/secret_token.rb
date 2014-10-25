d5ae52f96339064e5023f49835f80e4eb85cd70f
Diana Zink <dz@doerhub.com>
2014-10-25 14:49:08 -0700

    first commit with basic rails setup Users and Groups models on postgres

---
+++ peoplehuntr/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Peoplehuntr::Application.config.secret_key_base = '82a74499e11acdbcbe25819ec0c4e8f61997a60797b6428a6d62ed79dd41e85fae0e9a85b3a86a3e8a8e8331e271732746d4c7056000ce1bcc2af04ec4fd1817'
---
+++ peoplehuntr/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Peoplehuntr::Application.config.session_store :cookie_store, key: '_peoplehuntr_session'
