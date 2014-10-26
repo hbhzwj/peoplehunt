67391cde93959f7a4fba0b7f9aac7d5467dc5466
mannykary <mrkaryam@gmail.com>
2014-10-26 09:30:18 -0700

    updated findme python script to output json to text file

--- findme/findme/findme.py
+++ findme/findme/findme.py
@@ -25,7 +25,7 @@ GITHUB_URL = 'https://api.github.com/users/{0}/repos'
 with open('profiles.yaml', 'r') as profiles_file:
     profile_configs = yaml.load(profiles_file)
 
-print(profile_configs)
+#print(profile_configs)
 
 def get_profile(link):
     profile = None
@@ -92,7 +92,7 @@ def get_profiles(args):
     query = args['<query>']
 
     links = get_links(query)
-    print('Links: ', links)
+    #print('Links: ', links)
 
     if not links:
         return
@@ -108,7 +108,7 @@ def get_profiles(args):
 
 def get_links(text):
     query = SEARCH_QUERY.format(url_quote(text))
-    print('Query: ', query)
+    #print('Query: ', query)
 
     html = pg(requests.get(query).text)
     links = [a.attrib['href'] for a in html('.r')('a')]
@@ -148,6 +148,10 @@ def command_line_options():
                         profile[k] = current_value.extend(v)
 
     print(json.dumps(profile))
+    name = str(args['<query>'])
+    f1 = open(name.replace(" ", "_").replace('\n','') + '.json', 'w')
+    f1.write(json.dumps(profile) + '\n')
+    f1.close()
 
 if __name__ == '__main__':
     command_line_options()
